a54af8e424c8801d39d3a69c67b65b5fa6756d85
Felipe Alves <50992908+felipeAC98@users.noreply.github.com>
2023-01-08 18:45:50 -0300

    Add some new requests

--- deploy-lr-project/app.py
+++ deploy-lr-project/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
 import pickle
+import socket
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
+my_name = socket.gethostname()
 
 @app.route("/")
 def hello():
@@ -15,6 +17,16 @@ def predict():
     prediction = model.predict([[rooms, distance]])
     output = round(prediction[0], 2)
     return render_template('index.html', prediction_text=f'A house with {rooms} rooms per dwelling and located {distance} km to employment centers has a value of ${output}K')
+
+@app.route("/predict_id", methods=['POST'])
+def predict_id():
+    id_request = int(request.form['id_request'])
+    rooms = int(request.form['rooms'])
+    distance = int(request.form['distance'])
+    prediction = model.predict([[rooms, distance]])
+    output = round(prediction[0], 2)
+    return f'{my_name};{id_request};{output}'
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host='0.0.0.0')
